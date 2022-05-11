0779c725364e85c5bad7658671169d9ca2eb6e49
Fernando Rocha <duffe_fernando@hotmail.com>
2022-05-11 19:38:35 -0300

    Correção de Sintaxe na Classe MovimentacaoService.java

--- src/main/java/com/dio/santander/bankline/api/service/MovimentacaoService.java
+++ src/main/java/com/dio/santander/bankline/api/service/MovimentacaoService.java
@@ -33,7 +33,7 @@ public class MovimentacaoService {
         movimentacao.setTipo(novaMovimentacao.getTipo());
         movimentacao.setValor(valor);
 
-        Correntista correntista = correntistaRepository.findAllById(novaMovimentacao.getIdConta()).orElse(null);
+        Correntista correntista = correntistaRepository.findById(novaMovimentacao.getIdConta()).orElse(null);
         if (correntista != null){
             correntista.getConta().setSaldo(correntista.getConta().getSaldo() + valor);
             correntistaRepository.save(correntista);
